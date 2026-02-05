ed8026b1eb49c6210ab14612cedade98318171bf
Rohith-Tech-king <rohithtamilselvan14@gmail.com>
2026-02-05 00:37:57 -0800

    Add files via upload

--- program 1/Hello.java
+++ program 1/Hello.java
@@ -1,14 +1,35 @@
-package com.scsvmv.javalab;
+package com.scsvmv.javalab.hello;
+
+import java.util.Date;
 
 /*
- *simple Hello class
- *Always wishes Hello World
+ *Hello class
+ *Demonstrates method overloading and command-line arguments
  */
 
 public class Hello
 {
-  public void wish()
-  {
-    System.out.println("Hello World");
-  }
-}
+   //Default constructor
+     public Hello()
+     {
+     }
+
+   //Generic wish
+     public void wish()
+     {
+       System.out.println("Hello World");
+     }
+
+   //Overloaded wish method with name parameter 
+     public void wish(String name)
+     {
+       System.out.println("Hello"+name);
+     }
+
+   //wish with name and today's date
+     public void wishWithDate(String name)
+     {
+       Date today=new Date();
+       System.out.println("Hello"+name+"|Date:"+today);
+     }
+} 
